e56be5358e9ce02661cb3a5342aab9bd71f45d2e
nonon <nononcrust@gmail.com>
2025-02-11 12:55:17 +0900

    feed 수정

--- src/app/desktop/feed/_components/feed-list-item.tsx
+++ src/app/desktop/feed/_components/feed-list-item.tsx
@@ -4,6 +4,7 @@ import profileImage from "@/assets/images/chat-emoticon.webp";
 import { Avatar } from "@/components/ui/avatar";
 import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import { IconButton } from "@/components/ui/icon-button";
+import { formatToTimeAgo } from "@/lib/date";
 import {
   CodeIcon,
   EllipsisIcon,
@@ -18,10 +19,18 @@ import {
 type FeedListItemProps = {
   user: {
     nickname: string;
+    email: string;
+  };
+  content: string;
+  createdAt: string;
+  count: {
+    comment: number;
+    like: number;
+    repost: number;
   };
 };
 
-export const FeedListItem = ({ user }: FeedListItemProps) => {
+export const FeedListItem = ({ user, content, count, createdAt }: FeedListItemProps) => {
   return (
     <li className="flex gap-3 px-4 py-3">
       <Avatar>
@@ -31,23 +40,23 @@ export const FeedListItem = ({ user }: FeedListItemProps) => {
       <div className="flex flex-col">
         <span className="text-[15px] font-semibold">
           {user.nickname}
-          <span className="text-subtle ml-1 font-normal">@nononcrust.social</span>
+          <span className="text-subtle ml-1 font-normal">{user.email}</span>
           <span className="mx-1">·</span>
-          <span className="text-sub text-[13px] font-normal">1시간 전</span>
+          <span className="text-sub text-[13px] font-normal">{formatToTimeAgo(createdAt)}</span>
         </span>
-        <p className="text-[15px]">안녕하세요!!!</p>
+        <p className="text-[15px]">{content}</p>
         <div className="text-subtle mt-2 flex items-center gap-16">
           <span className="flex items-center gap-0.5 text-sm">
             <MessageCircleIcon size={16} />
-            16
+            {count.comment}
           </span>
           <span className="flex items-center gap-0.5 text-sm">
             <HeartIcon size={16} />
-            16
+            {count.like}
           </span>
           <span className="flex items-center gap-0.5 text-sm">
             <Repeat2Icon size={16} />
-            16
+            {count.repost}
           </span>
           <DropdownMenu>
             <DropdownMenu.Trigger asChild>
--- src/app/desktop/feed/page.tsx
+++ src/app/desktop/feed/page.tsx
@@ -1,61 +1,31 @@
+"use client";
+
 import { SearchInput } from "@/app/mobile/_components/search-input";
+import profileImage from "@/assets/images/nonon.png";
+import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { ChipButton } from "@/components/ui/chip-button";
-import { UsersRoundIcon } from "lucide-react";
+import { IconButton } from "@/components/ui/icon-button";
+import { UsersRoundIcon, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 import { FeedListItem } from "./_components/feed-list-item";
 
 export default function FeedPage() {
   return (
-    <div className="mx-auto flex w-full px-4 md:max-w-4xl">
+    <div className="mx-auto flex w-full md:max-w-4xl">
       <main className="border-border w-full py-16 md:border-x">
         <ul className="border-border divide-border flex flex-col divide-y border-y">
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
-          <FeedListItem user={{ nickname: "노논" }} />
+          {Array.from({ length: 10 }).map((_, index) => (
+            <FeedListItem key={index} {...FEED} />
+          ))}
         </ul>
       </main>
-      <aside className="sticky top-[56px] right-0 hidden h-[120px] min-w-[320px] flex-col px-6 lg:flex">
+      <aside className="sticky top-[56px] right-0 hidden h-[120px] w-[360px] shrink-0 flex-col px-6 lg:flex">
         <SearchInput className="mt-6" placeholder="검색하기" />
-        <div className="mt-4 flex flex-col">
-          <Button className="mt-4">
-            <UsersRoundIcon size={16} />
-            팔로우할 사람 찾기
-          </Button>
-        </div>
-        <div className="mt-4 flex flex-col">
-          <div className="flex flex-wrap gap-2">
-            {TRENDS.map((trend, index) => (
-              <ChipButton key={index} size="xsmall" variant="outlined">
-                {trend}
-              </ChipButton>
-            ))}
-          </div>
-        </div>
-        <div className="text-subtle mt-4 flex items-center gap-1 font-bold">
-          <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
-            피드백
-          </Link>
-          •
-          <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
-            이용약관
-          </Link>
-          •
-          <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
-            개인정보 처리방침
-          </Link>
-          •
-          <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
-            고객센터
-          </Link>
-        </div>
+        <Trends />
+        <FollowRecommendation />
+        <Links />
       </aside>
     </div>
   );
@@ -70,3 +40,125 @@ const TRENDS = [
   "알루미늄 25%",
   "발렌타인데이",
 ] as const;
+
+const Trends = () => {
+  const [show, setShow] = useState(true);
+
+  if (!show) return null;
+
+  return (
+    <div className="mt-4 flex flex-col">
+      <div className="mb-3 flex items-center justify-between">
+        <span className="font-semibold">트렌드</span>
+        <IconButton
+          size="xsmall"
+          aria-label="숨기기"
+          variant="ghost"
+          onClick={() => setShow(false)}
+        >
+          <XIcon size={16} />
+        </IconButton>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        {TRENDS.map((trend, index) => (
+          <ChipButton key={index} size="xsmall" variant="outlined">
+            {trend}
+          </ChipButton>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const FollowRecommendation = () => {
+  const [show, setShow] = useState(true);
+
+  if (!show) return null;
+
+  return (
+    <div className="mt-4 flex flex-col">
+      <div className="mb-3 flex items-center justify-between">
+        <span className="font-semibold">팔로우 추천</span>
+        <IconButton
+          size="xsmall"
+          aria-label="숨기기"
+          variant="ghost"
+          onClick={() => setShow(false)}
+        >
+          <XIcon size={16} />
+        </IconButton>
+      </div>
+      <ul className="flex flex-col gap-5">
+        <FollowRecommendationItem />
+        <FollowRecommendationItem />
+        <FollowRecommendationItem />
+        <FollowRecommendationItem />
+      </ul>
+      <div className="mt-2 flex flex-col">
+        <Button className="mt-4">
+          <UsersRoundIcon size={16} />
+          팔로우할 사람 찾기
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const FollowRecommendationItem = () => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  return (
+    <li className="flex items-center">
+      <Avatar>
+        <Avatar.Image src={profileImage.src} alt="프로필 이미지" />
+      </Avatar>
+      <div className="ml-2 flex flex-1 flex-col">
+        <span className="text-sm font-semibold">노논</span>
+        <span className="text-subtle text-[13px]">@nononcrust.social</span>
+      </div>
+      <ChipButton
+        size="small"
+        variant={isFollowing ? "primary" : "outlined"}
+        onClick={() => setIsFollowing(!isFollowing)}
+      >
+        {isFollowing ? "팔로잉" : "팔로우"}
+      </ChipButton>
+    </li>
+  );
+};
+
+const Links = () => {
+  return (
+    <div className="text-subtle mt-4 flex items-center gap-1 font-bold">
+      <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
+        피드백
+      </Link>
+      •
+      <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
+        이용약관
+      </Link>
+      •
+      <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
+        개인정보 처리방침
+      </Link>
+      •
+      <Link className="text-primary text-[13px] font-normal hover:underline" href="#">
+        고객센터
+      </Link>
+    </div>
+  );
+};
+
+const FEED = {
+  user: {
+    nickname: "노논",
+    email: "@nononcrust.social",
+  },
+  content: "안녕하세요!!!",
+  createdAt: "2025-02-11T03:53:06.584Z",
+  count: {
+    comment: 126,
+    like: 23,
+    repost: 8,
+  },
+};
