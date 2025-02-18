769d611ee195f8358369bcecefea02225d2b9c74
nonon <nononcrust@gmail.com>
2025-02-18 14:08:04 +0900

    Button 테스트 케이스 추가

--- src/components/ui/button.test.tsx
+++ src/components/ui/button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Link from "next/link";
 import { Button } from "./button";
 
@@ -21,5 +22,45 @@ describe("Button", () => {
     const link = screen.getByText("링크");
 
     expect(link).toBeInTheDocument();
+  });
+
+  test("[a11y] 버튼이 disabled인 경우 aria-disabled 속성이 true여야 합니다.", async () => {
+    render(<Button disabled>버튼</Button>);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveAttribute("aria-disabled", "true");
+  });
+
+  test("[a11y] Enter 키로 버튼을 클릭할 수 있어야 합니다.", async () => {
+    const onClick = vitest.fn();
+
+    render(<Button onClick={onClick}>버튼</Button>);
+
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button");
+
+    button.focus();
+
+    await user.keyboard("{Enter}");
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("[a11y] Space 키로 버튼을 클릭할 수 있어야 합니다.", async () => {
+    const onClick = vitest.fn();
+
+    render(<Button onClick={onClick}>버튼</Button>);
+
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button");
+
+    button.focus();
+
+    await user.keyboard(" ");
+
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
