6a3cebe1086ca35fb9cebb7d10e69a191e4a1288
nonon <nononcrust@gmail.com>
2025-03-12 14:31:55 +0900

    formatToTime 함수 수정

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -24,16 +24,11 @@ export const formatToTimeAgo = (isoDateString: string): string => {
 
 // 오후 8:30
 export const formatToTime = (isoDateString: string): string => {
-  const date = new Date(isoDateString);
-
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-
-  const ampm = hours < 12 ? "오전" : "오후";
-  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-
-  return `${ampm} ${formattedHours}:${formattedMinutes}`;
+  return new Date(isoDateString).toLocaleTimeString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
 };
 
 // 2025년 1월 22일
