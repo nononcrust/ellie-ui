72e50fb44bf748d1d614be9c196681e0a6f1eb1f
nonon <nononcrust@gmail.com>
2025-04-10 12:26:06 +0900

    link 수정

--- apps/web/src/configs/link.ts
+++ apps/web/src/configs/link.ts
@@ -1,6 +1,6 @@
 export const link = {
-  storybook: "https://design-system-storybook-nononcrusts-projects.vercel.app",
+  storybook: "https://ellie-ui-storybook-nononcrusts-projects.vercel.app",
   github: "https://github.com/nononcrust",
-  repository: "https://github.com/nononcrust/design-system",
+  repository: "https://github.com/nononcrust/ellie-ui",
   email: "nononcrust@gmail.com",
 } as const;
