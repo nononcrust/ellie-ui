50b58ff182bb0353d4f02a884a4612279c4890c2
nonon <nononcrust@gmail.com>
2025-04-15 12:24:10 +0900

    docs 스타일 수정

--- apps/web/src/app/layout.config.tsx
+++ apps/web/src/app/layout.config.tsx
@@ -11,7 +11,13 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
+        <svg
+          className="text-primary"
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-label="Logo"
+        >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
         Ellie UI
