f1b40822e24a95c11ae6ee314ce0f938b2004c70
nonon <nononcrust@gmail.com>
2025-04-15 17:42:29 +0900

    clack 패키지 추가

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -1,5 +1,12 @@
+import { select } from "@clack/prompts";
 import { Command } from "commander";
 
-export const init = new Command().name("init").action(() => {
-  console.log("Initializing...");
+export const init = new Command().name("init").action(async () => {
+  await select({
+    message: "사용 중인 프레임워크를 선택해주세요.",
+    options: [
+      { value: "nextjs", label: "Next.js" },
+      { value: "vite", label: "Vite" },
+    ],
+  });
 });
