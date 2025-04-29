5f7918e441332e93e3d9ed4d9326e1908f1ed32d
nonon <nononcrust@gmail.com>
2025-04-29 14:48:12 +0900

    랜딩 footer 추가

--- apps/web/src/app/(header)/(landing)/page.tsx
+++ apps/web/src/app/(header)/(landing)/page.tsx
@@ -1,12 +1,14 @@
 import { Examples } from "./_components/exmaples";
+import { Footer } from "./_components/Footer";
 import { Hero } from "./_components/hero";
 
 export default function LandingPage() {
   return (
     <div className="to-background min-h-dvh bg-gradient-to-t from-neutral-100 dark:from-neutral-900">
-      <main className="container pb-16">
+      <main className="container">
         <Hero />
         <Examples />
+        <Footer />
       </main>
     </div>
   );
