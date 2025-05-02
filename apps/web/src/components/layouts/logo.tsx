0c9f05d3dbba7787bdf45ca7c833ab8d5d16a2b4
nonon <nononcrust@gmail.com>
2025-05-02 17:38:49 +0900

    로고 수정

--- apps/web/src/components/layouts/logo.tsx
+++ apps/web/src/components/layouts/logo.tsx
@@ -1,9 +1,5 @@
-import { PawPrintIcon } from "lucide-react";
-
 export const Logo = () => {
   return (
-    <span className="from-primary flex size-[1.5rem] items-center justify-center rounded-full bg-gradient-to-r to-sky-300">
-      <PawPrintIcon className="fill-white text-white" size={14} />
-    </span>
+    <span className="from-primary flex size-[1.25rem] items-center justify-center rounded-full bg-gradient-to-r to-sky-300" />
   );
 };
