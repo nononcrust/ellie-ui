e6a327775a841c421becb399a3c434bb3fd1c055
nonon <dev.nonon@gmail.com>
2025-05-04 16:28:50 +0900

    버튼 스타일 수정

--- packages/core/src/components/button.tsx
+++ packages/core/src/components/button.tsx
@@ -12,19 +12,19 @@ export const buttonVariant = {
   primary: "bg-primary text-white hover:bg-primary-dark",
   primaryLow:
     "bg-primary-lighter text-primary hover:bg-primary-lighter-hover dark:bg-primary-darker dark:text-white dark:hover:bg-primary-darker-hover",
-  primaryOutlined: "bg-background border-border text-primary hover:bg-background-hover",
+  primaryOutlined: "bg-background border border-border text-primary hover:bg-background-hover",
   primaryLowOutlined:
-    "border-primary text-primary bg-primary-lighter hover:bg-primary-lighter-hover dark:bg-primary-darker dark:text-white dark:border-primary-dark dark:hover:bg-primary-darker-hover",
+    "border border-primary text-primary bg-primary-lighter hover:bg-primary-lighter-hover dark:bg-primary-darker dark:text-white dark:border-primary-dark dark:hover:bg-primary-darker-hover",
   secondary: "bg-secondary text-main hover:bg-secondary-dark",
   contained: "bg-neutral text-background hover:bg-neutral-dark",
-  outlined: "border-border text-main hover:bg-background-hover",
+  outlined: "border border-border text-main hover:bg-background-hover",
   ghost: "hover:bg-background-hover",
   error: "bg-error text-white hover:bg-error-dark",
 };
 
 const buttonVariants = tv({
   base: cn(
-    "inline-flex justify-center items-center gap-2 font-semibold outline-hidden border border-transparent rounded-md whitespace-nowrap text-foreground transition-colors",
+    "inline-flex justify-center items-center gap-2 font-semibold outline-hidden rounded-md whitespace-nowrap text-foreground transition-colors",
     "disabled:opacity-50 disabled:pointer-events-none",
   ),
   variants: {
