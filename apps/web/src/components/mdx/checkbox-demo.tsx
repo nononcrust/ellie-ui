b43c293a60813652ee6c7f40be153db8b7d105ef
nonon <nononcrust@gmail.com>
2025-05-22 14:10:38 +0900

    checkbox docs 추가

---
+++ apps/web/src/components/mdx/checkbox-demo.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Checkbox } from "@ellie-ui/core";
+import { useCheckbox } from "@ellie-ui/core/hooks";
+
+export const CheckboxDemo = () => {
+  return <Checkbox defaultChecked />;
+};
+
+export const CheckboxDemoWithLabel = () => {
+  return (
+    <Checkbox size="small" defaultChecked>
+      <Checkbox.Label>서비스 약관에 동의합니다.</Checkbox.Label>
+    </Checkbox>
+  );
+};
+
+export const CheckboxDemoSizes = () => {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <Checkbox size="small" defaultChecked>
+        <Checkbox.Label>서비스 약관에 동의합니다.</Checkbox.Label>
+      </Checkbox>
+      <Checkbox size="medium" defaultChecked>
+        <Checkbox.Label>서비스 약관에 동의합니다.</Checkbox.Label>
+      </Checkbox>
+      <Checkbox size="large" defaultChecked>
+        <Checkbox.Label>서비스 약관에 동의합니다.</Checkbox.Label>
+      </Checkbox>
+    </div>
+  );
+};
+
+export const CheckboxDemoDisabled = () => {
+  return <Checkbox defaultChecked disabled />;
+};
+
+export const CheckboxDemoInvalid = () => {
+  return <Checkbox defaultChecked aria-invalid />;
+};
+
+export const CheckboxDemoIndeterminate = () => {
+  return <Checkbox checked="indeterminate" />;
+};
+
+export const CheckboxDemoControlled = () => {
+  const checkbox = useCheckbox();
+
+  return <Checkbox checked={checkbox.checked} onChange={checkbox.onChange} />;
+};
