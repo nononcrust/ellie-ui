cc2c7ca231e6170f6a1c0719b12c4b3261ceefcd
nonon <nononcrust@gmail.com>
2025-06-16 14:40:41 +0900

    라이센스, 푸터 수정

--- apps/web/src/app/(header)/(landing)/_components/footer.tsx
+++ apps/web/src/app/(header)/(landing)/_components/footer.tsx
@@ -1,7 +1,15 @@
+import { link } from "@/configs/link";
+import Link from "next/link";
+
 export const Footer = () => {
   return (
     <footer className="flex items-center justify-center py-12">
-      <span className="text-subtle text-sm">© 2025. Sangho Shin</span>
+      <span className="text-subtle text-sm">
+        © 2025. Built by{" "}
+        <Link className="underline" href={link.github}>
+          nonon
+        </Link>
+      </span>
     </footer>
   );
 };
