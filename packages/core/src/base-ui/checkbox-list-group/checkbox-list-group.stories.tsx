c080037c9dbde5d95c37d8bc4051ac6a9aa628af
nonon <nononcrust@gmail.com>
2025-08-05 11:32:24 +0900

    CheckboxListGroup 컴포넌트 추가

---
+++ packages/core/src/base-ui/checkbox-list-group/checkbox-list-group.stories.tsx
@@ -0,0 +1,142 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { useCheckboxGroup } from "../../hooks";
+import { CheckboxListGroup } from "./checkbox-list-group";
+
+const meta = {
+  title: "base-ui/CheckboxListGroup",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof CheckboxListGroup>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const options = [
+  {
+    value: "1",
+    label: "동적 UI",
+    description: "조건을 걸어 서버에 동작할 수 있는 통신을 보낼 수 있어요.",
+  },
+  { value: "2", label: "조건 조회 UI", description: "조건을 설정하여 데이터를 조회할 수 있어요." },
+  {
+    value: "3",
+    label: "조건 조회 + 동적 UI",
+    description: "조건을 설정하여 데이터를 조회하고 추가 동작을 할 수 있어요.",
+  },
+] as const;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup defaultValue={["1"]}>
+          {options.map((option) => (
+            <CheckboxListGroup.Option key={option.value} value={option.value}>
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+              <CheckboxListGroup.Description>{option.description}</CheckboxListGroup.Description>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
+
+export const LabelOnly: Story = {
+  render: () => {
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup defaultValue={["1"]}>
+          {options.map((option) => (
+            <CheckboxListGroup.Option key={option.value} value={option.value}>
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
+
+export const Disabled: Story = {
+  render: () => {
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup defaultValue={["1"]} disabled>
+          {options.map((option) => (
+            <CheckboxListGroup.Option key={option.value} value={option.value}>
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+              <CheckboxListGroup.Description>{option.description}</CheckboxListGroup.Description>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
+
+export const OptionDisabled: Story = {
+  render: () => {
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup>
+          {options.map((option) => (
+            <CheckboxListGroup.Option
+              key={option.value}
+              value={option.value}
+              disabled={option.value === "2"}
+            >
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+              <CheckboxListGroup.Description>{option.description}</CheckboxListGroup.Description>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
+
+export const Error: Story = {
+  render: () => {
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup defaultValue={["1"]} aria-invalid>
+          {options.map((option) => (
+            <CheckboxListGroup.Option key={option.value} value={option.value}>
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+              <CheckboxListGroup.Description>{option.description}</CheckboxListGroup.Description>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
+
+export const Controlled: Story = {
+  render: () => {
+    const checkboxGroup = useCheckboxGroup({
+      entries: options.map((option) => option.value),
+      initialEntries: ["1"],
+    });
+
+    return (
+      <div className="min-w-[24rem]">
+        <CheckboxListGroup value={checkboxGroup.value} onChange={checkboxGroup.onChange}>
+          {options.map((option) => (
+            <CheckboxListGroup.Option
+              key={option.value}
+              value={option.value}
+              disabled={option.value === "2"}
+            >
+              <CheckboxListGroup.Label>{option.label}</CheckboxListGroup.Label>
+              <CheckboxListGroup.Description>{option.description}</CheckboxListGroup.Description>
+            </CheckboxListGroup.Option>
+          ))}
+        </CheckboxListGroup>
+      </div>
+    );
+  },
+};
---
+++ packages/core/src/base-ui/checkbox-list-group/checkbox-list-group.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { CheckboxGroup as CheckboxGroupBase } from "@base-ui-components/react";
+import { Checkbox as CheckboxBase } from "@base-ui-components/react/checkbox";
+import { CheckIcon } from "lucide-react";
+import { createContextFactory } from "../../lib/context";
+import { cn } from "../../lib/utils";
+
+type CheckboxGroupValue = string[] | readonly string[];
+
+type CheckboxListGroup<TValue extends CheckboxGroupValue> = Omit<
+  CheckboxGroupBase.Props,
+  "value" | "onValueChange" | "onChange"
+> & {
+  value?: TValue;
+  onChange?: (value: TValue) => void;
+};
+
+const CheckboxListGroup = <TValue extends CheckboxGroupValue>({
+  className,
+  children,
+  "aria-invalid": ariaInvalid,
+  value,
+  onChange,
+  ...props
+}: CheckboxListGroup<TValue>) => {
+  return (
+    <CheckboxListGroupContext value={{ ariaInvalid }}>
+      <CheckboxGroupBase
+        className={cn("grid gap-0.5", className)}
+        value={value as string[]}
+        onValueChange={onChange as (value: string[]) => void}
+        {...props}
+      >
+        {children}
+      </CheckboxGroupBase>
+    </CheckboxListGroupContext>
+  );
+};
+
+type CheckboxListGroupOptionProps = Omit<CheckboxBase.Root.Props, "className"> & {
+  className?: string;
+};
+
+const CheckboxListGroupOption = ({
+  className,
+  children,
+  ...props
+}: CheckboxListGroupOptionProps) => {
+  const { ariaInvalid } = useCheckboxListGroupContext();
+
+  return (
+    <CheckboxBase.Root
+      className={cn(
+        "hover:bg-background-100 flex items-center gap-4 rounded-md px-4 py-3",
+        "data-checked:bg-background-100",
+        "data-disabled:pointer-events-none data-disabled:opacity-50",
+        ariaInvalid && "border-error focus-visible:ring-ring-error data-checked:border-error",
+        className,
+      )}
+      {...props}
+    >
+      <CheckboxBase.Indicator
+        className={cn(
+          "bg-background text-background flex size-5 items-center justify-center rounded-[0.3125rem]",
+          "border-border shadow-xs outline-hidden shrink-0 border",
+          "data-checked:border-primary data-checked:bg-primary data-checked:text-white",
+          ariaInvalid && "data-checked:bg-error",
+        )}
+        keepMounted
+      >
+        <CheckIcon className="stroke-3 size-[0.875rem]" />
+      </CheckboxBase.Indicator>
+      <div className="flex flex-col items-start">{children}</div>
+    </CheckboxBase.Root>
+  );
+};
+
+type CheckboxListGroupLabelProps = React.ComponentPropsWithRef<"span">;
+
+const CheckboxListGroupLabel = ({ className, children, ...props }: CheckboxListGroupLabelProps) => {
+  return (
+    <span className={cn("text-[0.9375rem] font-medium", className)} {...props}>
+      {children}
+    </span>
+  );
+};
+
+type CheckboxListGroupDescriptionProps = React.ComponentPropsWithRef<"span">;
+
+const CheckboxListGroupDescription = ({
+  className,
+  children,
+  ...props
+}: CheckboxListGroupDescriptionProps) => {
+  return (
+    <span className={cn("text-sub text-start text-[0.8125rem]", className)} {...props}>
+      {children}
+    </span>
+  );
+};
+
+type CheckboxListGroupGroupContextValue = {
+  ariaInvalid?: boolean | "true" | "false" | "grammar" | "spelling" | undefined;
+};
+
+const [CheckboxListGroupContext, useCheckboxListGroupContext] =
+  createContextFactory<CheckboxListGroupGroupContextValue>("CheckboxListGroup");
+
+CheckboxListGroup.Option = CheckboxListGroupOption;
+CheckboxListGroup.Label = CheckboxListGroupLabel;
+CheckboxListGroup.Description = CheckboxListGroupDescription;
+
+export { CheckboxListGroup };
