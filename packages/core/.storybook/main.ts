0e464045d75b8e8783e41da31b1b59b0cb5f6f70
nonon <nononcrust@gmail.com>
2025-10-31 16:03:20 +0900

    storybook v10 마이그레이션

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,11 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
-  addons: ["@chromatic-com/storybook", "@storybook/addon-docs"],
+  addons: [getAbsolutePath("@chromatic-com/storybook"), getAbsolutePath("@storybook/addon-docs")],
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
 };
 export default config;
+
+function getAbsolutePath(value: string) {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
