3b1a1f0704ae5df6a73ad5a852d70ca9f24dae25
nonon <nononcrust@gmail.com>
2025-11-03 11:01:58 +0900

    fumadocs provider 임포트 수정

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@ellie-ui/core";
-import { RootProvider } from "fumadocs-ui/provider";
+import { RootProvider } from "fumadocs-ui/provider/next";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
